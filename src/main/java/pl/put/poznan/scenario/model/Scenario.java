5658cd49a85f00abc2436fc18b27478e5852e638
Koziok <69522734+Koziok@users.noreply.github.com>
2021-12-19 12:57:30 +0100

    Add steps to Scenario model

--- src/main/java/pl/put/poznan/scenario/model/Scenario.java
+++ src/main/java/pl/put/poznan/scenario/model/Scenario.java
@@ -6,21 +6,19 @@ public class Scenario {
     private String title;
     private List<String> actors;
     private List<String> system_actors;
+    private List<Step> steps;
 
-    public Scenario() {
-    }
+    public Scenario() { }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
-    public List<String> getActors() {
-        return actors;
-    }
+    public List<String> getActors() { return actors; }
 
     public List<String> getSystemActors() {
         return system_actors;
     }
+
+    public List<Step> getSteps () { return steps; }
 
     public void setTitle(String title) {
         this.title = title;
@@ -33,5 +31,7 @@ public class Scenario {
     public void setSystemActors(List<String> system_actors) {
         this.system_actors = system_actors;
     }
+
+    public void setSteps(List<Step> steps) { this.steps = steps; }
 }
 
