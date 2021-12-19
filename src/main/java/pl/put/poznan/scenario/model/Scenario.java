99a95d4d3b7b309b4d32db35ad54f9518217bd85
Sodment <sodment@gmail.com>
2021-12-19 13:54:37 +0100

    Add Step.java as a class for scenartio step counting

--- src/main/java/pl/put/poznan/scenario/model/Scenario.java
+++ src/main/java/pl/put/poznan/scenario/model/Scenario.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Scenario {
     private String title;
     private List<String> actors;
+    private List<Step> steps;
 
     public Scenario() {
     }
@@ -13,16 +14,20 @@ public class Scenario {
         return title;
     }
 
-    public List<String> getActors() {
-        return actors;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public List<String> getActors() {
+        return actors;
     }
 
     public void setActors(List<String> actors) {
         this.actors = actors;
     }
+
+    public List<Step> getSteps() { return steps; }
+
+    public void setSteps(List<Step> steps) { this.steps = steps; }
 }
 
---
+++ src/main/java/pl/put/poznan/scenario/model/Step.java
@@ -0,0 +1,23 @@
+package pl.put.poznan.scenario.model;
+
+import java.util.List;
+
+public class Step {
+    private Integer stepNum;
+    private String actor;
+    private List<String> contentList;
+
+    public Step() { }
+
+    public Integer getStepNum() { return stepNum; }
+
+    public void setStepNum(Integer stepNum) { this.stepNum = stepNum; }
+
+    public String getActor() { return actor; }
+
+    public void setActor(String actor) { this.actor = actor; }
+
+    public List<String> getContent() { return contentList; }
+
+    public void setContent(List<String> contentList) { this.contentList = contentList; }
+}
