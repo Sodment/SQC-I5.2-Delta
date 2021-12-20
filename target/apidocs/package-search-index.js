e819fa96b85d33b82837b7b1f1c0fbbd47924015
Sodment <sodment@gmail.com>
2021-12-20 14:56:06 +0100

    Add autogenerated apidocs and .jar

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.put.poznan.scenario.app"},{"l":"pl.put.poznan.scenario.logic"},{"l":"pl.put.poznan.scenario.logic.visitor"},{"l":"pl.put.poznan.scenario.model"},{"l":"pl.put.poznan.scenario.rest"}];updateSearchResults();
