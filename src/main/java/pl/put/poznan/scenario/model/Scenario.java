c05076d3062e8cd4ecb78361ac2a7ff78d308c8e
Sodment <sodment@gmail.com>
2021-12-20 11:23:09 +0100

    Cleanup after merge

--- src/main/java/pl/put/poznan/scenario/model/Scenario.java
+++ src/main/java/pl/put/poznan/scenario/model/Scenario.java
@@ -12,30 +12,25 @@ public class Scenario {
 
     public String getTitle() { return title; }
 
-    public List<String> getActors() { return actors; }
-
-    public List<String> getSystemActors() {
-        return system_actors;
-    }
-
-    public List<Step> getSteps () { return steps; }
-
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public List<String> getActors() {
-        return actors;
-    }
+    public List<String> getActors() { return actors; }
 
     public void setActors(List<String> actors) {
         this.actors = actors;
     }
 
-    public List<Step> getSteps() { return steps; }
+    public List<String> getSystemActors() {
+        return system_actors;
+    }
+
     public void setSystemActors(List<String> system_actors) {
         this.system_actors = system_actors;
     }
+
+    public List<Step> getSteps() { return steps; }
 
     public void setSteps(List<Step> steps) { this.steps = steps; }
 }
--- src/main/java/pl/put/poznan/scenario/model/Step.java
+++ src/main/java/pl/put/poznan/scenario/model/Step.java
@@ -6,30 +6,6 @@ import java.util.List;
  * Klasa reprezentuje pojedynczy krok w scenariuszu
  */
 public class Step {
-    private Integer stepNum;
-    private String actor;
-    private List<String> contentList;
-    private List<Integer> returnPath;
-
-    public Step() { }
-
-    public Integer getStepNum() { return stepNum; }
-
-    public void setStepNum(Integer stepNum) { this.stepNum = stepNum; }
-
-    public String getActor() { return actor; }
-
-    public void setActor(String actor) { this.actor = actor; }
-
-    public List<String> getContent() { return contentList; }
-
-    public void setContent(List<String> contentList) { this.contentList = contentList; }
-
-    public List<Integer> getReturnPath() { return returnPath; }
-
-    public void setReturnPath(List<Integer> returnPath) { this.returnPath = returnPath; }
-
-public class Step {
     private Integer stepNumber;
     private String actor;
     private List<String> content;
